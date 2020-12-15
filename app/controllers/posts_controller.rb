1d1573f5d6004025b6535e465879a81a4ab83c03
errorseiya <errorseiya@gmail.com>
2020-12-15 17:04:40 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,17 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index# メモを保存した後にトップページへリダイレクトされる
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked # 既読であるか否かを判定するプロパティを指定
+      post.update(checked: false) # 「update 更新」のActiveRecord
+    else
+      post.update(checked: true)
+    end
+    # 「update 更新」のActiveRecord
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
+
 end
