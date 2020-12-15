1097ccf10e4c979754e848911572feb8eaf83611
errorseiya <errorseiya@gmail.com>
2020-12-15 19:12:56 +0900

    Revert "修正版・既読機能のサーバーサイド実装"
    This reverts commit 4fdac8400c0b5a5d36f3fb9c1c8049ecee9cafc5.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 # get 'posts/new', to: 'posts#new' ←削除
   post 'posts', to: 'posts#create'
-  # get 'posts', to: 'posts#checked'# queryパラメーター
-  get 'posts/:id', to: 'posts#checked'# pathパラメーター
 end
 
 
