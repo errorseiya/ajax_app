4fdac8400c0b5a5d36f3fb9c1c8049ecee9cafc5
errorseiya <errorseiya@gmail.com>
2020-12-15 19:08:37 +0900

    修正版・既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,22 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index# メモを保存した後にトップページへリダイレクトされる
   end
+
+  
+  def checked
+    # 設定したURLパラメーターから、既読したメモのidが渡されるように設定するので、
+    # そのidを使用して該当するレコードを取得
+    post = Post.find(params[:id])
+    if post.checked # post.checkedという既読であるか否かを判定
+      post.update(checked: false)# 既読であれば「既読を解除するためにfalseへ変更」
+    else
+      post.update(checked: true)# 既読でなければ「既読にするためtrueへ変更」
+    end# update=更新 というActive record
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+    # ルーティングにリクエストを送ってきたjsファイルに勝手に送ってくれる
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 # get 'posts/new', to: 'posts#new' ←削除
   post 'posts', to: 'posts#create'
+  # get 'posts', to: 'posts#checked'# queryパラメーター
+  get 'posts/:id', to: 'posts#checked'# pathパラメーター
 end
 
 
